a145522ab053e4ab80f74bc7d3d66ce98af99a87
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-06-12 16:52:06 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import { DragSource } from 'react-dnd';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
